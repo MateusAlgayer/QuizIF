d65b469afe841f5a85148fce2fdacbd8d7b32ae8
Mateus Roberto Algayer <mateusr.algayer@gmail.com>
2021-12-30 19:41:17 -0300

    correção

--- QuizIFCliente/src/view/FormLogin.java
+++ QuizIFCliente/src/view/FormLogin.java
@@ -22,12 +22,12 @@ public class FormLogin extends javax.swing.JFrame{
 
     jLabel1 = new javax.swing.JLabel();
     jLabel2 = new javax.swing.JLabel();
-    lbTrocaSenha = new javax.swing.JLabel();
     btLogar = new javax.swing.JButton();
     btCadastro = new javax.swing.JButton();
     tfUsu = new javax.swing.JTextField();
     pfSenha = new javax.swing.JPasswordField();
     lbAviso = new javax.swing.JLabel();
+    jButton1 = new javax.swing.JButton();
 
     setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
     setTitle("Login");
@@ -37,13 +37,6 @@ public class FormLogin extends javax.swing.JFrame{
     jLabel1.setText("Usuário:");
 
     jLabel2.setText("Senha:");
-
-    lbTrocaSenha.setText("Esqueci minha senha!");
-    lbTrocaSenha.addMouseListener(new java.awt.event.MouseAdapter() {
-      public void mouseClicked(java.awt.event.MouseEvent evt) {
-        lbTrocaSenhaMouseClicked(evt);
-      }
-    });
 
     btLogar.setText("Logar");
     btLogar.addActionListener(new java.awt.event.ActionListener() {
@@ -66,30 +59,37 @@ public class FormLogin extends javax.swing.JFrame{
     lbAviso.setForeground(new java.awt.Color(255, 51, 51));
     lbAviso.setText("Usuário ou senha Incorretos!");
 
+    jButton1.setText("Esqueci minha senha");
+    jButton1.setBorder(null);
+
     javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
     getContentPane().setLayout(layout);
     layout.setHorizontalGroup(
       layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
       .addGroup(layout.createSequentialGroup()
-        .addGap(22, 22, 22)
-        .addComponent(jLabel1)
-        .addGap(12, 12, 12)
-        .addComponent(tfUsu, javax.swing.GroupLayout.PREFERRED_SIZE, 225, javax.swing.GroupLayout.PREFERRED_SIZE))
-      .addGroup(layout.createSequentialGroup()
-        .addGap(29, 29, 29)
-        .addComponent(jLabel2)
-        .addGap(12, 12, 12)
-        .addComponent(pfSenha, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE)
-        .addGap(12, 12, 12)
-        .addComponent(lbAviso, javax.swing.GroupLayout.PREFERRED_SIZE, 156, javax.swing.GroupLayout.PREFERRED_SIZE))
-      .addGroup(layout.createSequentialGroup()
-        .addGap(112, 112, 112)
-        .addComponent(lbTrocaSenha))
+        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+          .addGroup(layout.createSequentialGroup()
+            .addGap(22, 22, 22)
+            .addComponent(jLabel1)
+            .addGap(12, 12, 12)
+            .addComponent(tfUsu, javax.swing.GroupLayout.PREFERRED_SIZE, 225, javax.swing.GroupLayout.PREFERRED_SIZE))
+          .addGroup(layout.createSequentialGroup()
+            .addGap(29, 29, 29)
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+              .addComponent(jButton1)
+              .addGroup(layout.createSequentialGroup()
+                .addComponent(jLabel2)
+                .addGap(12, 12, 12)
+                .addComponent(pfSenha, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE)))
+            .addGap(12, 12, 12)
+            .addComponent(lbAviso, javax.swing.GroupLayout.PREFERRED_SIZE, 156, javax.swing.GroupLayout.PREFERRED_SIZE)))
+        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
       .addGroup(layout.createSequentialGroup()
         .addGap(43, 43, 43)
         .addComponent(btCadastro, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE)
-        .addGap(57, 57, 57)
-        .addComponent(btLogar, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE))
+        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        .addComponent(btLogar, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE)
+        .addGap(31, 31, 31))
     );
     layout.setVerticalGroup(
       layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -109,21 +109,18 @@ public class FormLogin extends javax.swing.JFrame{
             .addGap(13, 13, 13)
             .addComponent(pfSenha, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
           .addComponent(lbAviso, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE))
-        .addGap(6, 6, 6)
-        .addComponent(lbTrocaSenha)
-        .addGap(43, 43, 43)
-        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+        .addComponent(jButton1)
+        .addGap(37, 37, 37)
+        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
           .addComponent(btCadastro, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)
-          .addComponent(btLogar, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)))
+          .addComponent(btLogar, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE))
+        .addContainerGap(41, Short.MAX_VALUE))
     );
 
     pack();
     setLocationRelativeTo(null);
   }// </editor-fold>//GEN-END:initComponents
-
-  private void lbTrocaSenhaMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lbTrocaSenhaMouseClicked
-    //Abrir o form de troca de senha
-  }//GEN-LAST:event_lbTrocaSenhaMouseClicked
 
   private void btCadastroActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btCadastroActionPerformed
     //abrir o form de cadastro
@@ -151,10 +148,10 @@ public class FormLogin extends javax.swing.JFrame{
   // Variables declaration - do not modify//GEN-BEGIN:variables
   private javax.swing.JButton btCadastro;
   private javax.swing.JButton btLogar;
+  private javax.swing.JButton jButton1;
   private javax.swing.JLabel jLabel1;
   private javax.swing.JLabel jLabel2;
   private javax.swing.JLabel lbAviso;
-  private javax.swing.JLabel lbTrocaSenha;
   private javax.swing.JPasswordField pfSenha;
   private javax.swing.JTextField tfUsu;
   // End of variables declaration//GEN-END:variables
