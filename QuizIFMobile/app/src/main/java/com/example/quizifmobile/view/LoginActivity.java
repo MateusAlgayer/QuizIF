1b13277c1f55ddd2c2f9fa875bdc572cdb114dda
TheBleachGuy <amigodob2@gmail.com>
2022-02-14 11:09:22 -0300

    Tela de Login do k7 do android
    3 aulas pra fazer um botão k

--- QuizIFMobile/app/src/main/java/com/example/quizifmobile/view/LoginActivity.java
+++ QuizIFMobile/app/src/main/java/com/example/quizifmobile/view/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.quizifmobile.view;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -8,11 +9,18 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.quizifmobile.R;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText etSenhaUsu, etEmailUsu;
+
+    Button btCadUsu, btRecuperarSenha, btLogUsu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +28,19 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.login);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        etSenhaUsu = findViewById(R.id.etSenhaUsu);
+        etEmailUsu = findViewById(R.id.etEmailUsu);
+        btCadUsu = findViewById(R.id.btCadUsu);
+        btRecuperarSenha = findViewById(R.id.btRecuperarSenha);
+        btLogUsu = findViewById(R.id.btLogUsu);
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        btLogUsu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(LoginActivity.this, PrincipalActivity.class);
+                startActivity(it);
+            }
+        });
     }
 
 }
