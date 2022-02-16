556dae56ed0728d9d8d2b72a4ecd36c720d8b2e6
TheBleachGuy <amigodob2@gmail.com>
2022-02-16 11:01:21 -0300

    Login Mobile
    Login Mobile Funcional (provavelmente)

--- QuizIFMobile/app/src/main/java/com/example/quizifmobile/controller/ConexaoController.java
+++ QuizIFMobile/app/src/main/java/com/example/quizifmobile/controller/ConexaoController.java
@@ -1,5 +1,7 @@
 package com.example.quizifmobile.controller;
 
+import com.example.quizifmobile.util.CriptoHash;
+
 import ModelDominio.Prova;
 import ModelDominio.Usuario;
 
@@ -43,5 +45,37 @@ public class ConexaoController {
         }
 
         return null;
+    }
+
+    public Usuario login(Usuario pUsu) {
+        Usuario wUsu = null;
+
+        String msg = "";
+        try {
+            gravaLog("LOG", 0, "Efetuar login - INI");
+
+            out.writeObject("EFETUARLOGIN");
+
+            msg = (String) in.readObject();
+            out.writeObject(pUsu.getEmail());
+
+            String sal = (String)in.readObject();
+
+            if(sal.isEmpty()){
+                return null;
+            }
+
+            pUsu.setSenha(CriptoHash.Cripto(pUsu.getSenha(), sal, 0));
+
+            out.writeObject(pUsu);
+
+            wUsu = (Usuario) in.readObject();
+            gravaLog("LOG", 0, "Efetuar login - FIM");
+
+        } catch (IOException | ClassNotFoundException e) {
+            gravaLogErro("ERR", 0, "Erro ao enviar o form de login\n"+e.toString());
+        }
+
+        return wUsu;
     }
 }
--- QuizIFMobile/app/src/main/java/com/example/quizifmobile/view/LoginActivity.java
+++ QuizIFMobile/app/src/main/java/com/example/quizifmobile/view/LoginActivity.java
@@ -3,6 +3,8 @@ package com.example.quizifmobile.view;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.quizifmobile.controller.ConexaoController;
+import com.example.quizifmobile.controller.InfoApp;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
@@ -16,11 +18,15 @@ import android.widget.EditText;
 
 import com.example.quizifmobile.R;
 
+import ModelDominio.Usuario;
+
 public class LoginActivity extends AppCompatActivity {
 
     EditText etSenhaUsu, etEmailUsu;
 
     Button btCadUsu, btRecuperarSenha, btLogUsu;
+
+    InfoApp infoApp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,13 +40,32 @@ public class LoginActivity extends AppCompatActivity {
         btRecuperarSenha = findViewById(R.id.btRecuperarSenha);
         btLogUsu = findViewById(R.id.btLogUsu);
 
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        infoApp = (InfoApp) getApplicationContext();
         
         btLogUsu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent it = new Intent(LoginActivity.this, PrincipalActivity.class);
-                startActivity(it);
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Usuario wLogin = new Usuario(etEmailUsu.getText().toString(), etSenhaUsu.getText().toString());
+
+                        ConexaoController ccont = new ConexaoController(infoApp.in, infoApp.out);
+
+                        Usuario wUsu = ccont.login(wLogin);
+
+                        if (wUsu != null) {
+                            infoApp.setGUsuLogado(wUsu);
+
+                            Intent it = new Intent(LoginActivity.this, PrincipalActivity.class);
+                            startActivity(it);
+                        }
+                    }
+                }).start();
+
             }
         });
     }
