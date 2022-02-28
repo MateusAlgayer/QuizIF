a50c7f68ce434a936744993d84651a362a719d09
Mateus Roberto Algayer <mateusr.algayer@gmail.com>
2022-02-28 11:57:12 -0300

    confirma codigo email

--- QuizIFMobile/app/src/main/java/com/example/quizifmobile/view/ConfirmaCodigoEmailActivity.java
+++ QuizIFMobile/app/src/main/java/com/example/quizifmobile/view/ConfirmaCodigoEmailActivity.java
@@ -1,22 +1,26 @@
 package com.example.quizifmobile.view;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
-import android.view.View;
 
 import com.example.quizifmobile.R;
 
 
 public class ConfirmaCodigoEmailActivity extends AppCompatActivity {
 
+    //constantes
     final int TELAPERFIL = 1;
+    final int TELACADASTRO = 2;
+    final int TELALOGIN = 3;
+
+    int telaPai;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +29,59 @@ public class ConfirmaCodigoEmailActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
         Intent it = getIntent();
 
+        telaPai = it.getIntExtra("TelaPai", TELALOGIN);
+        final String acao = it.getStringExtra("Acao");
 
+        
     }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == android.R.id.home){
+            onBackPressed();
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        AlertDialog.Builder dialogSair = new AlertDialog.Builder(ConfirmaCodigoEmailActivity.this);
+        dialogSair.setMessage("Tem certeza que deseja sair?\n o progresso será perdido");
+        dialogSair.setCancelable(true);
+
+        dialogSair.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Intent it = retIntentPai();
+
+                startActivity(it);
+                finish();
+            }
+        });
+
+        dialogSair.setNegativeButton("Não", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        AlertDialog alertSair = dialogSair.create();
+        alertSair.show();
+    }
+
+    private Intent retIntentPai() {
+        switch(telaPai){
+            case TELAPERFIL: return new Intent(ConfirmaCodigoEmailActivity.this, PerfilActivity.class);
+            //case TELACADASTRO:return new Intent(ConfirmaCodigoEmailActivity.this, );
+            default: return new Intent(ConfirmaCodigoEmailActivity.this, LoginActivity.class);
+        }
+    }
 }
